02de8ed9d919dc68204b5ab4a4700e41307f7354
datahaki <jan.hakenberg@gmail.com>
2022-10-31 08:18:13 +0100

    show dialog

--- src/main/java/ch/alpine/ubongo/Candidates.java
+++ src/main/java/ch/alpine/ubongo/Candidates.java
@@ -1,19 +1,17 @@
 // code by jph
 package ch.alpine.ubongo;
 
-import java.awt.Dimension;
-import java.io.IOException;
 import java.util.List;
 
-import ch.alpine.bridge.fig.ListPlot;
+import ch.alpine.bridge.fig.ListLinePlot;
 import ch.alpine.bridge.fig.Show;
+import ch.alpine.bridge.fig.ShowDialog;
 import ch.alpine.tensor.Tensor;
 import ch.alpine.tensor.Tensors;
-import ch.alpine.tensor.ext.HomeDirectory;
 
 enum Candidates {
   ;
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) {
     Show show = new Show();
     for (int use = 1; use <= 12; ++use) {
       Tensor xy = Tensors.empty();
@@ -21,8 +19,8 @@ enum Candidates {
         List<List<Ubongo>> list = Ubongo.candidates(use, count);
         xy.append(Tensors.vectorInt(count, list.size()));
       }
-      show.add(ListPlot.of(xy)).setLabel("" + use);
+      show.add(ListLinePlot.of(xy)).setLabel("" + use);
     }
-    show.export(HomeDirectory.Pictures("ubongo_candidate_size.png"), new Dimension(800, 600));
+    ShowDialog.of(show);
   }
 }
